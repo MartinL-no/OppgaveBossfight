7a73d076b8daec278f4ee70327f69e490648fe8d
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-17 15:37:29 +0100

    completed exercise

---
+++ OppgaveBossfight/GameCharacter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OppgaveBossfight
+{
+    
+    public class GameCharacter
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Strength { get; private set; }
+        public int Stamina { get; private set; }
+        public int InitialStamina { get; private set; }
+        public bool Invulnerable { get; private set; }
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            InitialStamina = stamina;
+            Invulnerable = false;
+        }
+        public void Fight(GameCharacter opponent)
+        {
+            if (opponent.Invulnerable)
+            {
+                /* 
+                 * I have not reduced the attackers stamina while the other character is invulnerable
+                 * as the Boss always one with that algorithm.
+                 */
+                Console.WriteLine($"{opponent.Name} is invulnerable, {Name}'s attack caused no damage");
+                opponent.Invulnerable = false;
+            }
+            else if (Stamina == 0)
+            {
+                Console.WriteLine($"{Name} has no stamina, {opponent.Name} was not attacked");
+                Recharge();
+            }
+            else
+            {
+                opponent.Health = (opponent.Health - Strength) < 0 ? 0 : (opponent.Health - Strength);
+                Stamina  = (Stamina - 10) < 0 ? 0 : (Stamina - 10);
+
+                Console.WriteLine($"{Name} hit {opponent.Name} with {Strength} damage, {opponent.Name} now has {opponent.Health} health left.");
+            }
+        }
+        public void Recharge()
+        {
+            Stamina = InitialStamina;
+            Invulnerable = true;
+            Console.WriteLine($"{Name}'s stamina has been restored to what it was at the start of the fight ({InitialStamina}), {Name} will take no damage during the next attack)");
+        }
+    }
+}
+
--- OppgaveBossfight/Program.cs
+++ OppgaveBossfight/Program.cs
@@ -1,9 +1,50 @@
-﻿namespace OppgaveBossfight;
+﻿using System;
+using System.ComponentModel;
+
+namespace OppgaveBossfight;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var rand = new Random();
+        var randomStrength = rand.Next(31);
+        
+        var Hero = new GameCharacter("Hero", 100, 20, 40);
+        var Boss = new GameCharacter("Boss", 400, randomStrength, 10);
+
+        RunGame(Hero, Boss);
+    }
+    static void RunGame(GameCharacter Hero, GameCharacter Boss)
+    {
+        var rand = new Random();
+        bool isHeroesTurn = rand.Next(2) == 1;
+
+        while (true)
+        {
+            CharacterTurn(isHeroesTurn, Hero, Boss);
+
+            if (Hero.Health == 0)
+            {
+                Console.WriteLine($"{Boss.Name} is the winner!");
+                break;
+            }
+            else if (Boss.Health == 0)
+            {
+                Console.WriteLine($"{Hero.Name} is the winner!");
+                break;
+            }
+            isHeroesTurn = !isHeroesTurn;
+        }
+    }
+    static void CharacterTurn(bool isHeroesTurn, GameCharacter hero, GameCharacter boss)
+    {
+        if (isHeroesTurn)
+        {
+            hero.Fight(boss);
+        }
+        else
+        {
+            boss.Fight(hero);
+        }
     }
 }
-
